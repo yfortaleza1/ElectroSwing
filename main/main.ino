cf847f0763d515e7803b6bf9d80d1f4019449085
Jessica Turner <jturner3@umbc.edu>
2023-11-01 21:02:20 -0400

    Use Arduino IDE suggested path: main/main.ino

---
+++ main/main.ino
@@ -0,0 +1,14 @@
+/*
+* Project: Ava's Motorized Swing
+* Authors: Yoel Fortaleza, Alec Kalisch, Marc Conn, Jess Turner
+* Date: 10/28/2023
+*/
+#include <Stepper.h>
+
+void setup(){
+    
+}
+
+void loop(){
+
+}
