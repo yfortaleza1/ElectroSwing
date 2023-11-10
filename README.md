0ab6a7f1298998a999e2f4cf866509eae1ccb787
Jessica Turner <jturner3@umbc.edu>
2023-11-10 12:11:03 -0500

    Added ASCII (Shell) Timer Display MM:SS + Ignore executables

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ElectroSwing
 Codebase for Ava's Motorized Swing
+
+Sources:
+- basic timer terminal output: 
+https://www.scaler.com/topics/cpp-timer/
--- main/timer_interface.h
+++ main/timer_interface.h
@@ -1,7 +1,25 @@
 #ifndef TIMER_INTERFACE_H
 #define TIMER_INTERFACE_H
 
-// put whatever functions you think we'll need for interfacing here
+#include <ctime>
 
+// code that uses these libraries for outputing a nicely formatted timer
+// in the shell
+// is from https://www.scaler.com/topics/cpp-timer/
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+
+bool timer_run = false;
+int seconds = 0;
+int minutes = 0;
+// put whatever functions you think we'll need for interfacing here
+// todo: pin numbers for each button
+void increment_timer();
+void set_timer();
+void start_timer();
+void stop_timer();
+void display_time();
 
 #endif
