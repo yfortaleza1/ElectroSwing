731511152f2f7e39438a22cdc08bbc18d0e2be8d
yfortaleza1 <yoelfortaleza@gmail.com>
2024-04-01 11:36:40 -0400

    Added increment and countDown

--- main/primary_LCD/primary_LCD.ino
+++ main/primary_LCD/primary_LCD.ino
@@ -1,26 +1,32 @@
 //Arduino code for LCD timer display and buttons
 /*
 * Project: Ava's Motorized Swing
-* Author: Yoel Fortaleza (source: `SheCodes` bot)
+* Author: Yoel Fortaleza, Jess Turner (source: `SheCodes` bot)
 * Date:   Friday March 29, 2024
 * Notes: 
 Edited from original code via the source as of today.
 Source 
 https://www.youtube.com/watch?v=gBjHI8RJYGY
 */
-
-
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-//Define variables
-int currentTimeValue[4];
-int currentState = 0; //0 - timer is reset to 0, 1 - anything else
-int seconds = 0;
-int secondsTens = 0;
-int minutes = 0;
-int hours = 0;
+//Define button variables
+const short int incrementPin = 10;
+const short int decrementPin = 11;
+const short int startPin = 12;
+const short int stopPin = 13;
+const short int buttonPins[] = {incrementPin,decrementPin,startPin,stopPin}; //used for buttons
+
+//Define timer variable
+int secTicks = 0;
+int minTens = 0;
+int minOnes = 0;
+int secTens = 0;
+int secOnes = 0;
+
+const short int stopMotorPin = 8;
+short int motorStatus = 1;
 
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); //Set LCD address for 16 chars and 2 line display (need to check address of COM port)
@@ -28,37 +34,87 @@ LiquidCrystal_I2C lcd(0x27, 20, 4); //Set LCD address for 16 chars and 2 line di
 void setup(){
     lcd.init();
     lcd.backlight();
+    attachInterrupt(digitalPinToInterrupt(incrementPin),incrementTime, HIGH); 
+    attachInterrupt(digitalPinToInterrupt(decrementPin),countDown, CHANGE); 
+    attachInterrupt(digitalPinToInterrupt(stopPin), sendStopSignal, CHANGE);
 }
 
 void loop(){
-    lcd.setCursor(1,0);
-    lcd.print("Hello World!");
-    delay(3000);
-    lcd.clear();
-
-    lcd.setCursor(2,0);
-    lcd.print("This is an LCD");
-    lcd.setCursor(2,1);
-    lcd.print("screen test");
-    delay(3000);
-    lcd.clear();
-
+    
 }
 
 
 void incrementTime(){
+      cli();
+      if(minOnes < 9 && minOnes >= 0){
+        minOnes +=1;//increment minsOnes place
+      }
 
+      else if(minOnes == 9 && minTens != 5){//ONES PLACE OVERFLOW CONDITION
+        minOnes = 0;//set ones place of mines to 0
+        minTens += 1;//incrmeent tens place of minutes
+      }
+
+      sei();
 }
 
 void countDown(){
+    cli();
+      if(digitalRead(stopPin) != LOW){//IF STOP BUTTON PRESSED, CLEAR TIMER AND END SWING
+        //debounceButton(stopPin);
+        clearTimer();//clear timer
+      }
 
+      else{//IF STOP BUTTON NOT PRESSED, DECREMENT NORMALLY
+
+        if(secOnes != 0){
+          secOnes = secOnes - 1;//decrement secOnes, a second should have passed
+        }
+
+        else if(secOnes == 0 && secTens != 0){
+
+          secOnes = 9;
+          secTens -=1;//decrement sec tens
+        }
+
+        else if(secOnes == 0 && secTens == 0 && minOnes != 0){
+
+          minOnes -=1;
+          secTens = 5;
+          secOnes = 9;
+        }
+
+        else if(secOnes == 0 && secTens == 0 && minOnes == 0 && minTens != 0){
+
+          minTens -=1;
+          minOnes = 9;
+          secTens = 5;
+          secOnes = 9;
+        }
+      }
+
+      sei();
 }
 
 void showTime(){
     lcd.setCursor(14,3);
-    lcd.print(currentTimeValue[0]);
-    lcd.print(currentTimeValue[1]);
+    lcd.print(minTens);
+    lcd.print(minOnes);
     lcd.print(":");
-    lcd.print(currentTimeValue[2]);
-    lcd.print(currentTimeValue[3]);
+    lcd.print(secTens);
+    lcd.print(secOnes);
+}
+
+void clearTimer(){
+  secOnes = 0;
+  secTens = 0;
+  minOnes = 0;
+  minTens = 0;
+}
+
+void sendStopSignal(){
+    cli();
+    clearTimer();
+    motorStatus = 0;
+    sei();
 }
