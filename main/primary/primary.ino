dc1862728535ef59d0db0931e001f34ce78c2c17
yfortaleza1 <yoelfortaleza@gmail.com>
2024-04-04 20:10:49 -0400

    Adding functionality for counting down, changed some pinouts

--- main/primary/primary.ino
+++ main/primary/primary.ino
@@ -1,8 +1,8 @@
 /*
 Name: primary.ino
 Authors: Marc Conn
-(soon to be contributers - Yoel Fortaleza, Jess Turner)
-Date: 3/27 (planning on changing this current code to be LCD display rather than seven segment)
+(contributers - Yoel Fortaleza, Jess Turner)
+Date: 3/27-4/3 (planning on changing this current code to be LCD display rather than seven segment)
 Description: 
 This file will control the timing for the swing system.
 Todo: use master pin to signal to secondary that it can do its logic. 
@@ -12,207 +12,235 @@ Todo: use master pin to signal to secondary that it can do its logic.
 //THIS IS USING ATMEGA2560 
 //ONLY WORKS FOR THE MEGA REV3, THE ONE WE ARE PLANNING ON USING FOR THE FINAL PRODUCT
 //THIS CODE WON'T WORK ON THE UNO BECCAUSE OF DIFFERENT REGISTERS AND CLOCK SPEED (NEED TO CONFIRM)
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <math.h> // floor function.
+#include <TimerOne.h>
 
-const int sensorPin= 0;//The analog sensor is connected to analog pin 0 of the arduino
+//Define button variables
+const short int incrementPin = 11;
+const short int decrementPin = 10;
+const short int startPin = 2; //Interrupt capable pin
+const short int stopPin = 3; //Interrupt capable pin
+const short int masterPin = 6;
+const short int buttonPins[] = {incrementPin,decrementPin,startPin,stopPin};//used for buttons
 
-//ASSUMING THE CLOCK IS 8MHZ
+//Define global timer variables
+volatile int minutes = 0;
+volatile int seconds = 0;
+volatile bool countDownActive = false;
 
-short int secTicks = 0;//used to keep track when a second should occur, clock is 16MHZ, so count up to 16 mill and decremenet timer
-short int minTens = 0;//tens place for minutes
-short int minOnes = 0;//ones place for minutes
-short int secTens = 0;//tens place for seconds
-short int secOnes = 0;//ones place for seconds
+// Global variables for debounce
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-//BUTTON PINs
-const short int incrementPin = 22;//used for incrementing time
-const short int decrementPin = 23;//used for decremnting time
+// Variables to store the button states (this is for reading debounces)
+bool lastIncrementButtonState = HIGH;
+bool lastDecrementButtonState = HIGH;
+bool lastStartButtonState = HIGH;
+bool lastStopButtonState = HIGH;
 
+//Initialize LCD
+LiquidCrystal_I2C lcd(0x27, 20, 4); //Set LCD address for 16 chars and 2 line display (need to check address of COM port)
 
-//pins for decimal point and each segment
-//dp, G, F, E, D, C, B, A
-const short int segmentPins[]= { 2, 3, 4, 5, 6, 8, 11, 12};///used for 7 segment
-const short int buttonPins[] = {incrementPin,decrementPin,startPin,stopPin};;//used for buttons
-
-const short int numberofDigits=4;
-
-const short int digitPins[numberofDigits] = { 7,9,10,13}; //digits 1, 2, 3, 4
-
-
-
-//setup button pins
-void buttonSetup(){
-  for (int i=0; i < 4; i++){
-    pinMode(buttonPins[i], INPUT_PULLUP); //set segment and DP pins to output
-  }
-
-}
-
-
-
-//USED THIS FOR TRIGGERING SEGMENT DISPLAY
-//THIS IS USING TIMER0
-//16 bit timer
-void SevenSegTimerSetup() {
-  cli();
-  // Clear registers
-  TCCR0A = 0;
-  TCCR0B = 0;
-  TCNT0 = 0;
-
-  // 100.16025641025641 Hz (16000000/((155+1)*1024))
-  //period is 0.01 seconds
-  //OCR0A = 155;
-
-  // 744.047 Hz (16000000/((20+1)*1024))
-  //Period is 0.001344 seconds
-  OCR0A = 30;
-  // CTC
-  TCCR0A |= (1 << WGM01);
-  // Prescaler 1024
-  TCCR0B |= (1 << CS02) | (1 << CS00);
-  // Output Compare Match A Interrupt Enable
-  TIMSK0 |= (1 << OCIE0A);
-  sei();
-}
-
-
-
-
-
-//FUNCTION STEPS UP I/O PINS
-void segmentPinSetup(){
-  for (int i=0; i < 8; i++){
-    pinMode(segmentPins[i], OUTPUT); //set segment and DP pins to output
-  }
-  //sets the digit pins as outputs
-  for (int i=0; i < numberofDigits; i++){
-    pinMode(digitPins[i], OUTPUT);
-  }
-
-
-}
-
-
-
-
-
-  
-//USED FOR CLEARING SWING TIME
-void clearTimer(){
-  secOnes = 0;
-  secTens = 0;
-  minOnes = 0;
-  minTens = 0;
-}
-
-
-
-//this function is responsible for time decrementing logic
-//Function decrements timer by 1 SECOND
-//THIS FUNCTION WILL BE CALLED BY ISR EVERY SECOND TO GET THE TIMING RIGHT
-void decrementTimer(){
-      cli();
-      if(digitalRead(stopPin) != LOW){//IF STOP BUTTON PRESSED, CLEAR TIMER AND END SWING
-        //debounceButton(stopPin);
-        clearTimer();//clear timer
-      }
-
-      else{//IF STOP BUTTON NOT PRESSED, DECREMENT NORMALLY
-
-        if(secOnes != 0){
-          secOnes = secOnes - 1;//decrement secOnes, a second should have passed
-        }
-
-        else if(secOnes == 0 && secTens != 0){
-
-          secOnes = 9;
-          secTens -=1;//decrement sec tens
-        }
-
-        else if(secOnes == 0 && secTens == 0 && minOnes != 0){
-
-          minOnes -=1;
-          secTens = 5;
-          secOnes = 9;
-        }
-
-        else if(secOnes == 0 && secTens == 0 && minOnes == 0 && minTens != 0){
-
-          minTens -=1;
-          minOnes = 9;
-          secTens = 5;
-          secOnes = 9;
-        }
-      }
-
-      sei();
-}
-
-
-
-
-
-
-void incrementTimer(){
-
-      cli();
-      if(minOnes < 9 && minOnes >= 0){
-        minOnes +=1;//increment minsOnes place
-      }
-
-      else if(minOnes == 9 && minTens != 5){//ONES PLACE OVERFLOW CONDITION
-        minOnes = 0;//set ones place of mines to 0
-        minTens += 1;//incrmeent tens place of minutes
-      }
-
-      sei();
-}
-
-
+//Function declarations
+void buttonSetup();
+void incrementTime();
+void decrementTime();
+void startTime();
+void stopTime();
+void updateTime();
+void timerTick();
 
 void setup()
 {
-
- 
   //USED FOR TESTING
-  
-  minTens = 1;//tens place for minutes
-  minOnes = 3;//ones place for minutes
-  secTens = 1;//tens place for seconds
-  secOnes = 2;//ones place for seconds
-  
-  //cli();
+  buttonSetup();
+  clearTimer();
 
+  lcd.init();
+  lcd.backlight();
 
+  attachInterrupt(digitalPinToInterrupt(stopPin), stopTime, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(startPin), startTime, CHANGE);
 
-  attachInterrupt(digitalPinToInterrupt(incrementPin),incrementTimer, CHANGE); 
-  attachInterrupt(digitalPinToInterrupt(decrementPin),decrementTimer, CHANGE); 
-
-  //SevenSegTimerSetup();
-   //sei();
-   Serial.begin(9600);
-
+  //Setup using Timer library
+  Timer1.initialize(1000000); //Initialize TimerONe to 1 second intervals
+  Timer1.attachInterrupt(timerTick); //Attach timerTick function to the timer
+  Timer1.stop(); //Stop timer until explicityly started
+  Serial.begin(9600);
 }
-
-
-
 
 
 //MOTOR WILL KEEP OSCILLATING SO LONG AS THERE'S TIME REMAINING
 //ONCE TIME RUNS OUT THE SEVEN SEG SHOULD DISPLAY 00:00 FOREVER
 void loop(){
+	// Current reading from the input pin
+	bool currentIncrementState = digitalRead(incrementPin);
+	bool currentDecrementState = digitalRead(decrementPin);
+	bool currentStartState = digitalRead(startPin);
+	bool currentStopState = digitalRead(stopPin);
 
+	// Check if the button state has changed (button press or release)
+	if (currentIncrementState != lastIncrementButtonState) {
+	// reset the debouncing timer
+	lastDebounceTime = millis();
+	}
 
-  Serial.println("HEY");
+	if (currentDecrementState != lastDecrementButtonState) {
+	lastDebounceTime = millis();
+	}
 
+	if (currentStartState != lastStartButtonState) {
+	lastDebounceTime = millis();
+	}
 
+	if (currentStopState != lastStopButtonState) {
+	lastDebounceTime = millis();
+	}
 
+	// Check if the debounce period has passed (the button state has changed)
+	if ((millis() - lastDebounceTime) > debounceDelay) {
+	// Take action only if the button was pressed (assuming active LOW)
+	if (currentIncrementState == LOW) {
+		incrementTime();
+	}
 
+	if (currentDecrementState == LOW) {
+		decrementTime();
+	}
+
+	if (currentStartState == LOW) {
+		startTime();
+	}
+
+	if (currentStopState == LOW) {
+		stopTime();
+	}
+	}
+
+	// Save the reading. Next time through the loop, it'll be the lastButtonState:
+	lastIncrementButtonState = currentIncrementState;
+	lastDecrementButtonState = currentDecrementState;
+	lastStartButtonState = currentStartState;
+	lastStopButtonState = currentStopState;
+
+	if (digitalRead(incrementPin) == HIGH) {
+	incrementTime();
+	delay(200); // debounce delay
+	}
+
+	if (digitalRead(decrementPin) == HIGH) {
+	decrementTime();
+	delay(200); // debounce delay
+	}
+
+	updateTime();
 }
 
 
 
+/*
+Helper functions and ISRs
+*/
+
+void timerTick(){
+	if(countDownActive){
+		decrementTime();
+		if(minutes == 0 && seconds == 0){
+			stopTime(); //Stop the timer if it reaches 0
+		}
+	}
+}
 
 
+void startTime(){
+	if(!countDownActive){
+		countDownActive = true;
+		digitalWrite(masterPin, HIGH); //Send output signal as HIGH
+		Timer1.resume(); //Resume timer interrupt for decrementing the countdown
+	}
 
+}
+
+void stopTime(){
+	countDownActive = false;
+	Timer1.stop(); //Stop the timer interrupt;
+	digitalWrite(masterPin, LOW);
+}
+
+
+void timerInterrupt(){
+
+}
+
+//setup button pins
+void buttonSetup(){
+	for (int i=0; i < 4; i++){
+	pinMode(buttonPins[i], INPUT_PULLUP); //set segment and DP pins to output
+	}
+	pinMode(masterPin, OUTPUT);
+	digitalWrite(masterPin, LOW);
+}
+
+
+//USED FOR CLEARING SWING TIME
+void clearTimer(){
+  minutes = 0;
+  seconds = 0;
+}
+
+
+//this function is responsible for time decrementing logic
+//Function decrements timer by 1 SECOND
+//THIS FUNCTION WILL BE CALLED BY ISR EVERY SECOND TO GET THE TIMING RIGHT
+void decrementTime(){
+	Serial.println("AH YOU PUSHED DECREMENT :0 +++++++++++++ ");
+	noInterrupts(); //Disable interrupts
+
+	if(minutes > 0){
+		minutes--;
+	}
+	interrupts();
+}
+
+
+void incrementTime(){
+    Serial.println("AH YOU PUSHED INCREMENT :0 +++++++++++++ ");
+    noInterrupts(); //Disable interrupts
+	minutes++; //Increment minutes
+
+	if(minutes >= 100){ //Check if minutes exceed 99
+		minutes = 0;
+	}
+	interrupts();
+}
+
+void updateTime(){
+	lcd.clear();
+  	lcd.setCursor(0,0);
+	lcd.print("********************");
+
+	lcd.setCursor(0,1);
+	lcd.print("**** SWING TIMER ****");
+
+	lcd.setCursor(0,2);
+	if(minutes > 10){
+		lcd.print("******* ");
+		lcd.print("0");
+		lcd.print(minutes);
+		lcd.print(":");
+		lcd.print(seconds);
+		lcd.print(" *******");
+	}else{
+		lcd.print("******* ");
+		lcd.print(minutes);
+		lcd.print(":");
+		lcd.print(seconds);
+		lcd.print(" *******");
+	}
+
+	lcd.setCursor(0,3);
+	lcd.print("********************");
+}
 
